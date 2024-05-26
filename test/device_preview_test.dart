fded29cf8ad4a7f8234c0910f6b9286e1c1747ab
Joshua Tang <joshuaystang@gmail.com>
2024-05-26 19:43:56 +1000

    feat: update to support flutter 3.22 (#4)

---
+++ test/device_preview_test.dart
@@ -0,0 +1,16 @@
+import 'package:device_preview_plus/device_preview.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  testWidgets('render widget', (tester) async {
+    await tester.pumpWidget(
+      DevicePreview(
+        builder: (context) => const MaterialApp(),
+      ),
+    );
+    await tester.pumpAndSettle();
+
+    expect(find.byType(DevicePreview), findsOneWidget);
+  });
+}
