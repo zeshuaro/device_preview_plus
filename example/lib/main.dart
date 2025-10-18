9076d0dd95edb71fe518ee8188d7812ec5ae434f
Done Developer <donedeveloper.info@gmail.com>
2025-10-18 12:26:39 +0200

    feat(example): only enable preview in debug mode
    The `DevicePreview` in the example app is now only enabled when running in debug mode by checking the `kDebugMode` flag. This prevents it from being active in release builds.

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:device_preview_plus/device_preview_plus.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 import 'basic.dart';
@@ -8,7 +9,7 @@ import 'custom_plugin.dart';
 void main() {
   runApp(
     DevicePreview(
-      enabled: true,
+      enabled: (kDebugMode)?true:false,
       tools: const [...DevicePreview.defaultTools, CustomPlugin()],
       builder: (context) => const BasicApp(),
     ),
